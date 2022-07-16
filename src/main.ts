94c79be51a8c286c465323d089e8a1741022a950
antoniosk10 <antoniosk10@gmail.com>
2022-07-16 05:39:11 +0500

    feat: add openAPI/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  SwaggerModule.setup('doc', app, document);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
