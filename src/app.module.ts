65a6ccd720966a3c27908555e0e4539b0431736c
Anton Skarabahaty <antoniosk10@gmail.com>
2022-08-02 17:48:38 +0500

    refactor: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,25 @@ import { ArtistsModule } from './artists/artists.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import configService from './ormconfig';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
+import { AtGuard } from './common/guards';
 
 @Module({
   imports: [
+    AuthModule,
     UsersModule,
     TracksModule,
     AlbumsModule,
     ArtistsModule,
     FavoritesModule,
     TypeOrmModule.forRoot(configService),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
   ],
 })
 export class AppModule {}
